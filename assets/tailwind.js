9eafe1d490614f7240172a9cc6fed2d5c6badd53
ChangJoo Park <pcjpcj2@gmail.com>
2019-10-13 15:41:27 +0900

    tailwind base  설정 추가

--- assets/tailwind.js
+++ assets/tailwind.js
@@ -1,6 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ["-apple-system", "BlinkMacSystemFont"],
+      serif: ["Noto Serif KR", "Palatino Linotype", "Palatino", "Palladio", "URW Palladio L", "Georgia", "serif"],
+      mono: ["SFMono-Regular", "Menlo"],
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+    }
   },
   variants: {},
   plugins: []
