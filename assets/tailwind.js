6e6c835d166f3d55e08197a96f4addb96664e287
ChangJoo Park <pcjpcj2@gmail.com>
2019-10-13 16:15:12 +0900

    Update Noto Sans KR

--- assets/tailwind.js
+++ assets/tailwind.js
@@ -1,7 +1,7 @@
 module.exports = {
   theme: {
     fontFamily: {
-      sans: ["-apple-system", "BlinkMacSystemFont"],
+      sans: ["Noto Sans KR", "-apple-system", "BlinkMacSystemFont"],
       serif: ["Noto Serif KR", "Palatino Linotype", "Palatino", "Palladio", "URW Palladio L", "Georgia", "serif"],
       mono: ["SFMono-Regular", "Menlo"],
     },
