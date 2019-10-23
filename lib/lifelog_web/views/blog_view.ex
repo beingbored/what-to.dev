f477fabeb0ab4df41a3625612d1ee8b1df92fdc0
ChangJoo Park(박창주) <changjoo.park@iplateia.com>
2019-10-23 22:09:57 +0900

    TODO: ecto datetime to timex parsed string

--- lib/lifelog_web/views/blog_view.ex
+++ lib/lifelog_web/views/blog_view.ex
@@ -4,7 +4,7 @@ defmodule LifelogWeb.BlogView do
 
   def render_markdown(body) do
     {:ok, html, _} = Earmark.as_html(body)
-    raw html
+    raw(html)
   end
 
   def title("index.html", _assigns), do: "Blog - LifeLog"
@@ -12,4 +12,9 @@ defmodule LifelogWeb.BlogView do
   def title("new.html", _assigns), do: "새 글 - LifeLog"
   def title(_other, _assigns), do: "LifeLog"
 
+  # TODO: ecto_time 을 timex 로 parse 하는 법을 알아야함.
+  # https://en.it1352.com/article/b59e6110740f4b279f1cafdf841afa05.html
+  def humanizeDateTime(ecto_time) do
+    ecto_time
+  end
 end
