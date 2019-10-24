1b91a989bb358cc7dc80dee9cc5ca3f3fd041d44
ChangJoo Park(박창주) <changjoo.park@iplateia.com>
2019-10-24 20:12:34 +0900

    add changeset to signup path

--- lib/lifelog_web/controllers/user_controller.ex
+++ lib/lifelog_web/controllers/user_controller.ex
@@ -1,8 +1,12 @@
 defmodule LifelogWeb.UserController do
     use LifelogWeb, :controller
 
+    alias Lifelog.Accounts
+    alias Lifelog.Items.User
+
     def signup(conn, _params) do
-        render(conn, "signup.html")
+        changeset = Accounts.change_user(%User{})
+        render(conn, "signup.html", changeset: changeset)
     end
 
     def create(conn, _params) do
