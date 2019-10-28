866c47629d4305f2272d1bc3aeacf8b0fbacac0d
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-10-28 19:10:46 +0900

    Add user created

--- lib/lifelog_web/controllers/user_controller.ex
+++ lib/lifelog_web/controllers/user_controller.ex
@@ -1,17 +1,24 @@
 defmodule LifelogWeb.UserController do
-    use LifelogWeb, :controller
+  use LifelogWeb, :controller
 
-    alias Lifelog.Accounts
-    alias Lifelog.Items.User
+  alias Lifelog.Accounts
+  alias Lifelog.Items.User
 
-    def signup(conn, _params) do
+  def signup(conn, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "signup.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created!")
+        |> redirect(to: Routes.session_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = cs} ->
         changeset = Accounts.change_user(%User{})
         render(conn, "signup.html", changeset: changeset)
     end
-
-    def create(conn, %{"user" => user_params}) do
-        IO.inspect(user_params)
-        changeset = Accounts.create_user(%User{})
-        render(conn, "signup.html", changeset: changeset)
-    end
+  end
 end
