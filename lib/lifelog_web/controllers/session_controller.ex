07540e95780876c23685e1b23ae39693824d2930
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-10-28 19:56:03 +0900

    Add example for session guard

--- lib/lifelog_web/controllers/session_controller.ex
+++ lib/lifelog_web/controllers/session_controller.ex
@@ -5,6 +5,15 @@ defmodule LifelogWeb.SessionController do
     alias Lifelog.Items.User
 
     def index(conn, _params) do
+        # if (conn.assigns.current_user) do
+        #     conn
+        # else
+        #     changeset = Accounts.change_user(%User{})
+        #     conn
+        #     |> put_flash(:error, "You must sign in!")
+        #     |> redirect(to: Routes.page_path(conn, :index))
+        #     |> halt()
+        # end
         changeset = Accounts.change_user(%User{})
         render(conn, "index.html", changeset: changeset)
     end
