d51f659bde7cf583031a8b78a2d34d459de918db
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-10-29 21:24:05 +0900

    Add writing status for blog form

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,50 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
+const updateStatus = debounce((event) => {
+    const sentences = event.target.value
+        .split('\n')
+        .filter(p => p.trim().length >= 1)
+    const words = sentences
+        .map(sentence => sentence.split(' '))
+        .reduce((acc,curr) => acc + curr.length, 0)
+    status.innerText = `Sentences: ${sentences.length}, Words: ${words}`
+}, 100)
+
+const blogFormPageHandler = ()  => {
+    const textarea = document.getElementById('blog-post-body')
+    const status = document.getElementById('blog-form-body-status')
+    if (!textarea || !status) { return }
+    textarea.addEventListener("keyup", updateStatus)
+}
+const removeBlogFormEvent = () => {
+    const textarea = document.getElementById('blog-post-body')
+    const status = document.getElementById('blog-form-body-status')
+    if (!textarea || !status) { return }
+    textarea.removeEventListener("keyup", () => updateStatus)
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (document.getElementById('blog-new')) {
+        blogFormPageHandler();
+    } else {
+        removeBlogFormEvent();
+    }
+})
