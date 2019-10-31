8fbd2d7c2c0472ac772e3a67cdb2ca716eda7106
ChangJoo Park(박창주) <changjoo.park@iplateia.com>
2019-10-31 21:26:36 +0900

    using liveview

--- lib/lifelog_web.ex
+++ lib/lifelog_web.ex
@@ -36,6 +36,9 @@ defmodule LifelogWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
+      # Use Phoenix Framework LiveView
+      import Phoenix.LiveView, only: [live_render: 2, live_render: 3]
+
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
@@ -54,6 +57,8 @@ defmodule LifelogWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+
+      import Phoenix.LiveView.Router
     end
   end
 
