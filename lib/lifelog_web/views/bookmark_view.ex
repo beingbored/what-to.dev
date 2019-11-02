4699e9515a1d54006623ba2071ef3c5e67cad459
ChangJoo Park(박창주) <changjoo.park@iplateia.com>
2019-11-02 20:05:23 +0900

    Update placeholder

--- lib/lifelog_web/views/bookmark_view.ex
+++ lib/lifelog_web/views/bookmark_view.ex
@@ -3,7 +3,7 @@ defmodule LifelogWeb.BookmarkView do
 
     alias LifelogWeb.BookmarkLive
 
-    def bookmark_image(%{ "image_link": image, "icon_link": icon}) when image == ""  and icon == "", do: "https://via.placeholder.com/300"
+    def bookmark_image(%{ "image_link": image, "icon_link": icon}) when image == ""  and icon == "", do: "https://via.placeholder.com/600x200/?text=no_image"
     def bookmark_image(%{ "image_link": image, "icon_link": icon}) when image != "", do: image
     def bookmark_image(%{ "image_link": image, "icon_link": icon}) when icon != "",  do: icon
     def bookmark_image(%{ "image_link": image, "icon_link": icon}) when image == "", do: icon
