e35d2a61d5a50137b886ab7e83aac2a542e0e16d
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-11-04 10:29:33 +0900

    implement edit

--- lib/lifelog_web/controllers/bookmark_controller.ex
+++ lib/lifelog_web/controllers/bookmark_controller.ex
@@ -1,15 +1,30 @@
 defmodule LifelogWeb.BookmarkController do
-    use LifelogWeb, :controller
-    alias Lifelog.Repo
-    alias Lifelog.Bookmarks
-    alias Phoenix.LiveView
-    alias LifelogWeb.BookmarkLiveView
+  use LifelogWeb, :controller
+  alias Lifelog.Repo
+  alias Lifelog.Bookmarks
+  alias Phoenix.LiveView
+  alias LifelogWeb.BookmarkLiveView
 
-    def index(conn, _params) do
-      LiveView.Controller.live_render(
-        conn,
-        BookmarkLiveView,
-        session: %{current_user: conn.assigns.current_user}
-      )
+  def index(conn, _params) do
+    LiveView.Controller.live_render(
+      conn,
+      BookmarkLiveView,
+      session: %{current_user: conn.assigns.current_user}
+    )
+  end
+
+  def edit(conn, %{"id" => id}) do
+    if conn.assigns.current_user do
+      bookmark = Bookmarks.get(id)
+      render(conn, "edit.html", %{bookmark: bookmark})
+    else
+      conn
+      |> put_flash(:error, "Please Sign In!")
+      |> redirect(to: Routes.session_path(conn, :new))
+      |> halt()
     end
+  end
+
+  def update(conn, _params) do
+  end
 end
