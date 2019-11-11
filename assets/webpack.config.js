2324552b02caf1f19c1e30f67296242271cc7a65
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-11-11 09:16:35 +0900

    remove purgecss settings

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,32 +4,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-
-// Custom PurgeCSS extractor for Tailwind that allows special characters in
-// class names.
-//
-// https://github.com/FullHuman/purgecss#extractor
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-  }
-}
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
       new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({}),
-      new PurgecssPlugin({
-        paths: glob.sync('../lib/<YOUR_APP_NAME>_web/templates/**/*.html.eex'),
-        extractors: [
-          {
-            extractor: TailwindExtractor,
-            extensions: ['html', 'js', 'eex'],
-          },
-        ],
-      }),
+      new OptimizeCSSAssetsPlugin({})
     ]
   },
   entry: {
